fad31cabe24e45f9c70b28e48571b5ebe00376bd
Kim Sandberg <s@ndberg.dk>
2022-01-20 00:42:50 +0100

    Renderer done.

--- Krig/View/Renderer.cs
+++ Krig/View/Renderer.cs
@@ -1,14 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Krig.Model;
 
 namespace Krig.View
 {
     internal class Renderer
     {
+        private string _topLine = "********************************************************\n";
+        private int _lMargin = 1;
+
         internal Renderer()
         {
 
@@ -16,34 +16,96 @@ namespace Krig.View
 
         internal StringBuilder createGameScreen(ref Player player1, ref Player player2, int cardsLeft)
         {
-            // Screen er 52 * lang
             StringBuilder screen = new();
-            int card1TextLength = player1.cardDrawn.color.ToString().Length + player1.cardDrawn.color.ToString().Length;
-            int card2TextLength = player2.cardDrawn.color.ToString().Length + player2.cardDrawn.color.ToString().Length;
+            int lengthOfUsableArea = _topLine.Length - 2;
 
-            screen.Append("*****************************************************\n");
+            // Længde på kort tekst for spiller 1 og spiller 2
+            int card1TextLength = player1.cardDrawn.color.ToString().Length + player1.cardDrawn.name.ToString().Length;
+            int card2TextLength = player2.cardDrawn.color.ToString().Length + player2.cardDrawn.name.ToString().Length;
 
-            if (player1.pointsFromRound==2)
+            // Toplinje
+            screen.Append(_topLine);
+
+            // Højre og venstre side.
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+
+            // Antal Omgange tilbage
+            string omgangeTekst = "Omgange tilbage: ";
+
+            screen.Append("*").Append(tab(_lMargin)).Append(omgangeTekst).Append(cardsLeft)
+                .Append(tab(lengthOfUsableArea - (omgangeTekst.Length + cardsLeft.ToString().Length + 2)))
+                .Append("*\n");
+
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+
+            // Spiller 1 linje
+            screen.Append("*").Append(tab(_lMargin))
+                .Append(player1.name).Append(" ").Append(player1.points).Append(" point")
+                //                             1. halvdel af skærmen - (faste char + 1 + variable char)  + 1 pga. den anden del af teksten er lang mangler 1 space.
+                .Append(tab(lengthOfUsableArea / 2 - (15 + player1.points.ToString().Length)))
+                .Append("Spillet kort: ")
+                .Append(player1.cardDrawn.color).Append(" ")
+                .Append(player1.cardDrawn.name)
+                //                             2. halvdel af skærmen - (faste char - 1 + variable char) - 1 pga. Der nu er en ekstra plads pga. +1 i linje 44
+                .Append(tab(lengthOfUsableArea / 2 - (14 + card1TextLength)))
+                .Append("*\n");
+
+            if (player1.pointsFromRound == 2)
             {
-                screen.Append("* Kort tilbage: ").Append(cardsLeft).Append(tab(37 - (16 + cardsLeft.ToString().Length))).Append("Vundet").Append(tab(7));
+                screen.Append("*").Append(tab(_lMargin))
+                    .Append(tab(lengthOfUsableArea / 2))
+                    .Append("vundet")
+                    //                             2. halvdel af skærmen - (faste char + 1 + variable char)  + 1 pga. den anden del af teksten er lang mangler 1 space.
+                    .Append(tab(lengthOfUsableArea / 2 - (7)))
+                    .Append("*\n");
             }
-             else
-             {
-                 screen.Append("* Kort tilbage: ").Append(cardsLeft)
-                     .Append(tab(51 - (16 + cardsLeft.ToString().Length)));
-             }
-             screen.Append("*\n");
+            else
+            {
+                screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            }
 
-            screen.Append("* H A L. ").Append(player1.points).Append(" point").Append(tab(23 - (15 + player1.points.ToString().Length)))
-                .Append("Spillet kort: ").Append(player1.cardDrawn.color).Append(" ").Append(player1.cardDrawn.name)
-                .Append(tab(25 - (15 + card1TextLength))).Append(" *\n");
+            // Uafgjort
+            if (player1.pointsFromRound == player2.pointsFromRound)
+            {
+                screen.Append("*").Append(tab(_lMargin))
+                    .Append(tab(lengthOfUsableArea/2))
+                    .Append("uafgjort")
+                    //                             2. halvdel af skærmen - (faste char + 1 + variable char)  + 1 pga. den anden del af teksten er lang mangler 1 space.
+                    .Append(tab(lengthOfUsableArea / 2 - (9)))
+                    .Append("*\n");
+            }
+            else // almindelig
+            {
+                screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            }
 
-            screen.Append("*                                                   *\n");
-            screen.Append("*                                                   *\n");
+            if(player2.pointsFromRound == 2)
+            {
+                screen.Append("*").Append(tab(_lMargin))
+                    .Append(tab(lengthOfUsableArea / 2))
+                    .Append("vundet")
+                    //                             2. halvdel af skærmen - (faste char + 1 + variable char)  + 1 pga. den anden del af teksten er lang mangler 1 space.
+                    .Append(tab(lengthOfUsableArea / 2 - (7)))
+                    .Append("*\n");
+            }
+            else
+            {
+                screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            }
 
-            screen.Append("* Menneske. ").Append(player2.points).Append(" point").Append(tab(22 - (17 + player2.points.ToString().Length)))
-                .Append("Spillet kort: ").Append(player2.cardDrawn.color).Append(" ").Append(player2.cardDrawn.name)
-                .Append(tab(25 - (15 + card2TextLength))).Append(" *\n");
+            // Spiller 1 linje
+            screen.Append("*").Append(tab(_lMargin))
+                .Append(player1.name).Append(" ").Append(player2.points).Append(" point")
+                //                             1. halvdel af skærmen - (faste char + 1 + variable char)  + 1 pga. den anden del af teksten er lang mangler 1 space.
+                .Append(tab(lengthOfUsableArea / 2 - (15 + player2.points.ToString().Length)))
+                .Append("Spillet kort: ")
+                .Append(player2.cardDrawn.color).Append(" ")
+                .Append(player2.cardDrawn.name)
+                //                             2. halvdel af skærmen - (faste char - 1 + variable char) - 1 pga. Der nu er en ekstra plads pga. +1 i linje 44
+                .Append(tab(lengthOfUsableArea / 2 - (14 + card2TextLength)))
+                .Append("*\n");
+
+            screen.Append(_topLine);
 
 
             return screen;
@@ -52,18 +114,41 @@ namespace Krig.View
         internal StringBuilder createWonGameScreen(Player player)
         {
             StringBuilder screen = new();
+            int lengthOfUsableArea = _topLine.Length - 2;
+
+            screen.Append(_topLine);
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+
+
 
             // Hvis spillernummeret er 3 er kampen endt uafgjort
             if (player.playerNumber != 3)
             {
-                screen.Append("Spiller ").Append(player.name).Append(" vandt med ").Append(player.points)
-                    .Append(" point.");
+                int tekstLength = 27 + player.name.Length + player.points.ToString().Length;
+                screen.Append("*")
+                    .Append(tab(10))
+                    .Append("Spiller ").Append(player.name).Append(" vandt med ").Append(player.points).Append(" point.")
+                    .Append(tab(lengthOfUsableArea - ((10 + tekstLength))))
+                    .Append("*\n");
             }
-            else
+            else // uafgjort
             {
-                screen.Append("Kampen endte uafgjort mellem ").Append(player.name).Append(" med ").Append(player.points)
-                    .Append(" point.");
+                int tekstLength = 36;
+                screen.Append("*")
+                    .Append(tab(10))
+                    .Append("Kampen endte uafgjort med ").Append(player.points).Append(" point.")
+                    .Append(tab(lengthOfUsableArea - ((10 + tekstLength))))
+                    .Append("*\n");
             }
+
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            screen.Append("*").Append(tab(lengthOfUsableArea - 1)).Append("*\n");
+            screen.Append(_topLine);
+
             return screen;
         }
 
@@ -77,9 +162,9 @@ namespace Krig.View
         private string tab(int nrOfSpaces)
         {
             string spaces = "";
-            for (int counter = 0; counter <= nrOfSpaces; counter++)
+            for (int counter = 0; counter < nrOfSpaces; counter++)
             {
-                spaces += " ";
+                spaces = spaces + " ";
             }
             return spaces;
         }
